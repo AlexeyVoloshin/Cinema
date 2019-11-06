8150e5787121e415f05e03775113deec9e0240ed
AlexeyVoloshin <voloshinalex1100@gmail.com>
2019-11-06 17:51:34 +0200

    add new interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CinemaHallComponent } from './cinema-hall/cinema-hall.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CinemaHallComponent
+    CinemaHallComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
